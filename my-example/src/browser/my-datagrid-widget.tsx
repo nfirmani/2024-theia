7efb75012cf75114bc571c0e981d3055fb3540ff
nfirmani <nelson.firmani@gmail.com>
2020-10-26 11:26:57 +0000

    add material table

--- my-example/src/browser/my-datagrid-widget.tsx
+++ my-example/src/browser/my-datagrid-widget.tsx
@@ -3,9 +3,9 @@ import { injectable, postConstruct, inject } from 'inversify';
 import { AlertMessage } from '@theia/core/lib/browser/widgets/alert-message';
 import { ReactWidget } from '@theia/core/lib/browser/widgets/react-widget';
 import { MessageService } from '@theia/core';
-
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import { TableContainer, Table, TableBody, TableCell, TableHead, TableRow, Paper  } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -18,7 +18,58 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-export default function BasicTextFields() {
+const useStyles1 = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+export function DenseTable() {
+  const classes = useStyles1();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export  function BasicTextFields() {
   const classes = useStyles();
 
   return (
@@ -67,6 +118,9 @@ export class MyDatagridWidget extends ReactWidget {
                 <button>Browser...</button>
 
                  <BasicTextFields />
+                 <DenseTable />
+
+                 
             </div>
         </div>
     }
